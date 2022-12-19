d741ac3894b8fc11e79150a93ddeef8969fb9c93
Maujr3456 <93012770+Maujr3456@users.noreply.github.com>
2022-12-19 19:15:51 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+import * as React from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import DrawerNavigator from "./navigation/DrawerNavigation";
+
+export default function App() {
+  return <DrawerNavigator />;
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
